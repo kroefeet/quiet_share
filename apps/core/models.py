d06b8f9a5d37136ec05514efb24bb29f76923a9c
Joe Cera <subscribeto@runbox.com>
2019-07-19 14:47:52 -0700

    WeTransfer API is working

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class FilePost(models.Model):
+    username = models.CharField(max_length=30)
+    text = models.TextField(null=True, blank=True)
+    link = models.CharField(max_length=150)
+    created = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+
+    # def __str__(self):
+    #     return self.username + ' said ' + self.text
--- apps/core/views.py
+++ apps/core/views.py
@@ -1,4 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django import forms
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib import auth
+from py3wetransfer import Py3WeTransfer
+import os
+wetransfer_api_key = os.environ["WeTransfer_API_KEY"]
+from .models import FilePost
+
+# Two example views. Change or delete as necessary.
+
+class QuietShareForm(forms.Form):
+    text = forms.CharField(widget=forms.Textarea)
+    username = forms.CharField(max_length=30)
+    filename = forms.FileField(label = "Select a file")
 
 # Two example views. Change or delete as necessary.
 def home(request):
@@ -15,3 +30,33 @@ def about(request):
 
     return render(request, 'pages/about.html', context)
 
+def file(request):
+    if request.method == 'POST':
+                # Create a form instance and populate it with data from the request
+        form = QuietShareForm(request.POST)
+        x = Py3WeTransfer(wetransfer_api_key)
+
+#        if form.is_valid():
+        filename=request.POST['filename']
+        text=request.POST['text']
+        link = x.upload_file(filename, text)
+
+        filepost = FilePost.objects.create(
+            username=request.POST['username'],
+            text=text,
+            link = link,
+        )
+
+            # As soon as our new user is created, we make this user be
+            # instantly "logged in"
+            #auth.login(request, user)
+        return redirect('/')
+
+    else:
+        # if a GET we'll create a blank form
+        form = QuietShareForm()
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'pages/file.html', context)
