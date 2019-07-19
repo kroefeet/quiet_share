d05906325924ba2d7a2288dd5dc66ad27ed28bb0
Joe Cera <subscribeto@runbox.com>
2019-07-19 13:51:30 -0700

    first

---
+++ quiet_share/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for quiet_share project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quiet_share.config.production')
+
+application = get_asgi_application()
