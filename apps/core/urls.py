b3175d9f6d2a343f9c2d7a6181081ea83e00292d
Joe Cera <subscribeto@runbox.com>
2019-07-22 19:06:21 -0700

    getting started with dashboard view

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     # path('about/', views.about, name='about'),
     path('file/', views.file, name='file'),
+    path('user/', views.user_page, name='user_page'),
     # path('login/', views.login, name='login'),
     # path('signup/', views.signup, name='signup')
 ]
