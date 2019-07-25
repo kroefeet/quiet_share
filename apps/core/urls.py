03717ccd7d112b68a7ae8ea7ed20547974b209f6
Joe Cera <subscribeto@runbox.com>
2019-07-25 16:44:26 -0700

    trying to sort out heroku issues

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -5,5 +5,5 @@ from apps.core import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('<username>/', views.user_page, name='dashboard'),
-    path('delete-profile/<username>', views.delete_profile),
+    #path('delete-profile/<username>', views.delete_profile),
 ]
