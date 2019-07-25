47437eba95c39ae74bd3a0b81347487650266e91
Joe Cera <subscribeto@runbox.com>
2019-07-24 20:17:00 -0700

    getting api to work with heroku

--- quiet_share/config/production.py
+++ quiet_share/config/production.py
@@ -1,9 +1,8 @@
 # Settings that are unique to production go here
 from .base import *  # noqa
 
-DEBUG = False
+DEBUG = TRUE
 
 # Configure Django App for Heroku.
 import django_heroku
 django_heroku.settings(locals())
-
